a30a55f1924cdbf141d47d0a49e6971a9a8a7f67
shzgfx <tomaszkepa10@gmail.com>
2021-03-14 14:28:22 +0100

    1 file modified

--- assets/fetchData.js
+++ assets/fetchData.js
@@ -2,10 +2,20 @@ const API_KEY = "k_fg9k4x78";
 const API_URL = "https://imdb-api.com/en/API/SearchMovie/";
 
 export class FetchData {
-  constructor() {}
+  constructor() {
+    this.subscribers = [];
+  }
   fetchData(input) {
     fetch(`${API_URL}${API_KEY}/${input}`)
       .then(response => response.json())
-      .then(data => console.log(data));
+      .then(data => this.notifySubscribers(data));
+  }
+
+  addSubscriber(subscriber) {
+    this.subscribers.push(subscriber);
+  }
+
+  notifySubscribers(data) {
+    this.subscribers.forEach(subscriber => subscriber(data));
   }
 }
